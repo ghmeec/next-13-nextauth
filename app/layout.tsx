84567b70855c2a1f34dc88d5282759db0923c90f
ghmeec <39438375+ghmeec@users.noreply.github.com>
2023-06-15 14:14:33 +0400

    defining next auth secrets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import { Toaster } from 'react-hot-toast';
 
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -17,8 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-
-      <body className={inter.className}>
+      <body className={""}>
         <Toaster />
         {children}
       </body>
